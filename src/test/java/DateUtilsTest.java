67d5098e9a7488e9fcccb49eeee775bd0ca4a869
FaniryKeziah <hei.keziah.2@gmail.com>
2023-01-18 12:59:35 +0100

    add test dependecy

---
+++ src/test/java/DateUtilsTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DateUtilsTest {
+    @Test
+    void test_one_equals_one(){
+        assertEquals(1,1);
+    }
+
+}
