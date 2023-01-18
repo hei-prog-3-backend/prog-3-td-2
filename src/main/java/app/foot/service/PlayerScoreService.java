40b9c4659de5e0a81593ad409ea6b5b5686ed39b
Cedric <99885260+Cenikl@users.noreply.github.com>
2023-01-18 08:12:23 +0300

    chore: endpoint exception

--- src/main/java/app/foot/service/PlayerScoreService.java
+++ src/main/java/app/foot/service/PlayerScoreService.java
@@ -11,7 +11,9 @@ import app.foot.repository.entity.PlayerEntity;
 import app.foot.repository.entity.PlayerScoreEntity;
 import app.foot.repository.mapper.MatchMapper;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,20 +30,24 @@ public class PlayerScoreService {
 
     public List<PlayerScoreEntity> createPlayerScorer(Integer matchId, List<PlayerScorer> playerScorers){
 
+        if(matchId == null ){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"matchId should be an integer or not null");
+        }
+
         List<PlayerScoreEntity> playerScoreEntities = new ArrayList<>();
 
         MatchEntity match = matchRepository.findById(matchId).get();
         if(match == null){
-            throw new IllegalArgumentException("The Id of the Match doesn't exist");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Match " + match + " not found");
         }
 
         for(int i = 0; i < playerScorers.size(); i++){
             PlayerEntity playerEntity = playerRepository.findById(playerScorers.get(i).getPlayer().getId()).get();
             if(playerEntity.isGuardian()){
-                throw new RuntimeException("A guardian should not score");
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"A guardian should not score");
             }
             else if (playerScorers.get(i).getMinute() < 0 || playerScorers.get(i).getMinute() > 90) {
-                throw new RuntimeException("Scoring time should be between 0 and 90");
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Scoring time should be between 0 and 90");
             }
             else {
             playerScoreEntities.add(
