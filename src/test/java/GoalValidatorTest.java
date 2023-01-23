c1dc81c75f610c0ef48f286f328f853dde924cbe
Ryan Andriamahery <ryan@hei.school>
2023-01-23 13:21:48 +0300

    chore(to-do): complete GoalValidator and PlayerMapper tests

---
+++ src/test/java/GoalValidatorTest.java
@@ -0,0 +1,32 @@
+import app.foot.controller.validator.GoalValidator;
+import org.junit.jupiter.api.Test;
+
+//TODO-1: complete these tests
+public class GoalValidatorTest {
+    GoalValidator subject = new GoalValidator();
+
+    @Test
+    void accept_ok() {
+    }
+
+    //Mandatory attributes not provided : scoreTime
+    @Test
+    void accept_ko() {
+
+    }
+
+    @Test
+    void when_guardian_throws_exception() {
+
+    }
+
+    @Test
+    void when_score_time_greater_than_90_throws_exception() {
+
+    }
+
+    @Test
+    void when_score_time_less_than_0_throws_exception() {
+
+    }
+}
---
+++ src/test/java/PlayerMapperTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+//TODO-2: complete these tests
+public class PlayerMapperTest {
+    @Test
+    void player_to_domain_ok() {
+
+    }
+
+    @Test
+    void player_scorer_to_domain_ok() {
+
+    }
+
+    @Test
+    void player_scorer_to_entity_ok() {
+
+    }
+}
