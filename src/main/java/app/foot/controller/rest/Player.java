5d399f7f7969e9ec74be261ccb282d21865d073e
Ryan Andriamahery <ryan@hei.school>
2023-01-29 23:50:26 +0300

    chore: add player integration test with GET /players

--- src/main/java/app/foot/controller/rest/Player.java
+++ src/main/java/app/foot/controller/rest/Player.java
@@ -1,16 +1,15 @@
 package app.foot.controller.rest;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Builder(toBuilder = true)
+@ToString
+@EqualsAndHashCode
 public class Player {
-  private Integer id;
-  private String name;
-  private Boolean isGuardian;
+    private Integer id;
+    private String name;
+    private Boolean isGuardian;
 }
---
+++ src/test/java/integration/PlayerIntegrationTest.java
@@ -0,0 +1,78 @@
+package integration;
+
+import app.foot.FootApi;
+import app.foot.controller.rest.Player;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest(classes = FootApi.class)
+@AutoConfigureMockMvc
+class PlayerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    Player player1() {
+        return Player.builder()
+                .id(1)
+                .name("J1")
+                .isGuardian(false)
+                .build();
+    }
+
+    Player player2() {
+        return Player.builder()
+                .id(2)
+                .name("J2")
+                .isGuardian(false)
+                .build();
+    }
+
+    Player player3() {
+        return Player.builder()
+                .id(3)
+                .name("J3")
+                .isGuardian(false)
+                .build();
+    }
+
+    @Test
+    void read_players_ok() throws Exception {
+        MockHttpServletResponse response = mockMvc
+                .perform(get("/players"))
+                .andReturn()
+                .getResponse();
+        List<Player> actual = convertFromHttpResponse(response);
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals(9, actual.size());
+        assertTrue(actual.containsAll(List.of(
+                player1(),
+                player2(),
+                player3())));
+    }
+
+    private List<Player> convertFromHttpResponse(MockHttpServletResponse response)
+            throws JsonProcessingException, UnsupportedEncodingException {
+        CollectionType playerListType = objectMapper.getTypeFactory()
+                .constructCollectionType(List.class, Player.class);
+        return objectMapper.readValue(
+                response.getContentAsString(),
+                playerListType);
+    }
+}
