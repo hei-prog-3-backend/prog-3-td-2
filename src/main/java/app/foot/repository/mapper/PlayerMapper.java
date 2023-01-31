eec5540deb0d4d19b1140e0dc156066fe0e273b0
Jonas4884 <hei.jonatana@gmail.com>
2023-01-31 11:01:28 +0300

    chore(finished) : all playerMapperTest passed

--- src/main/java/app/foot/repository/mapper/PlayerMapper.java
+++ src/main/java/app/foot/repository/mapper/PlayerMapper.java
@@ -1,5 +1,6 @@
 package app.foot.repository.mapper;
 
+import app.foot.exception.NotFoundException;
 import app.foot.model.Player;
 import app.foot.model.PlayerScorer;
 import app.foot.repository.MatchRepository;
@@ -7,6 +8,7 @@ import app.foot.repository.PlayerRepository;
 import app.foot.repository.TeamRepository;
 import app.foot.repository.entity.PlayerEntity;
 import app.foot.repository.entity.PlayerScoreEntity;
+import app.foot.repository.entity.TeamEntity;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -44,11 +46,17 @@ public class PlayerMapper {
     }
 
     //TODO: add unit test ok and ko for this
-    public PlayerEntity toEntity(Player domain) {
+    public PlayerEntity toEntity(Player domain){
+
+        TeamEntity team = teamRepository.findByName(domain.getTeamName());
+
+        if(team==null){
+            throw new NotFoundException("Team name not found");
+        }
         return PlayerEntity.builder()
                 .id(domain.getId())
                 .name(domain.getName())
-                .team(teamRepository.findByName(domain.getTeamName()))
+                .team(team)
                 .guardian(domain.getIsGuardian())
                 .build();
     }
