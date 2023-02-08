7a89d3e5cae19fd27733c06f7848144e97f04eb1
Ryan Andriamahery <ryan@hei.school>
2023-02-08 08:39:47 +0300

    chore: ping test ok

---
+++ src/test/java/integration/HealthIntegrationTest.java
@@ -0,0 +1,30 @@
+package integration;
+
+import app.foot.FootApi;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = FootApi.class)
+@AutoConfigureMockMvc
+public class HealthIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void ping_ok() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(get("/ping"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse();
+
+        assertEquals("pong", response.getContentAsString());
+    }
+}
